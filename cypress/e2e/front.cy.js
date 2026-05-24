2b5d2774bf15dc2d2b93e08ab86184b443711c30
Ignacio Martin <tchfau@gmail.com>
2026-05-24 13:11:42 -0300

    Agrega caso de prueba front (Bianca)

--- cypress/e2e/front.cy.js
+++ cypress/e2e/front.cy.js
@@ -1,10 +1,9 @@
 describe('Casos de prueba de FRONT', () => {
 
   it.only('Comprar carrito exitosamente y visualizar orden de compra', () => {
-    //Accion paso 1:
     cy.visit('https://app.bookdbqa.online/login')
-    cy.get('input[formcontrolname="username"]').type('Automata')
-    cy.get('input[formcontrolname="password"]').type('Automata123')
+    cy.get('input[formcontrolname="username"]').type('UTest')
+    cy.get('input[formcontrolname="password"]').type('Utest123')
     cy.get('app-login button').contains('Login').click()
 
     //Respuesta del sistema paso 1:
@@ -21,11 +20,52 @@ describe('Casos de prueba de FRONT', () => {
 
     //Accion paso 3:
     cy.get('.mdc-icon-button.mat-mdc-icon-button.mat-mdc-button-base.mat-unthemed').contains('shopping_cart').click()
+
+    //Respuesta del sistema paso 3:
+    // Asegurar que el cambio de URL se completó
+    cy.url().should('include', '/shopping-cart')
+
+    //Acción paso 4:
+    cy.get('.mdc-button__label').contains('CheckOut').click()
+
+    //Respuesta del sistema
+    cy.url().should('include', 'https://app.bookdbqa.online/checkout')
+
+    // Acción paso 5:
+    cy.get('input[formcontrolname="name"]').type('Juan')
+    cy.get('input[formcontrolname="addressLine1"]').type('Av. Independencia 2057')
+    cy.get('input[formcontrolname="addressLine2"]').type('Av. Congreso 5965')
+    cy.get('input[placeholder="Pincode"]').type('123456')
+    cy.get('input[formcontrolname="state"]').type('Buenos Aires')
+
+    //Acción paso 6
+    cy.get('button[type="submit"]').contains('Place Order').click()
+
+    //Respuesta del sistema
+    cy.url().should('include', 'https://app.bookdbqa.online/myorders')
+
+    //Accion paso 7
+    cy.get('tr.mat-mdc-row').first().click()
+
+    // Respuesta del sistema - Verificación del detalle de la orden:
     
+    // 1. Validamos que el contenedor del detalle de la orden se hizo visible
+    cy.get('mat-card.mat-mdc-card').first().should('be.visible')
+    
+    // 2. Validamos que la primera tabla de detalles esté presente en la pantalla
+    cy.get('table.details-table').first().should('be.visible')
 
-
-
-
+    // 3. Agregamos .first() aquí para que .within() trabaje sobre un solo elemento
+    cy.get('table.details-table').first().within(() => {
+      // Valida que el título del libro sea el correcto
+      cy.contains('Harry Potter and the Chamber of Secrets').should('be.visible')
+      
+      // Valida que la cantidad especificada en el detalle sea 1
+      cy.contains('1').should('be.visible')
+      
+      // Valida que figure el monto pagado correspondiente
+      cy.contains('₹236.00').should('be.visible')
+    })
   })
 
   it('Titulo caso de prueba 2 | Nombre Alumno', () => {
